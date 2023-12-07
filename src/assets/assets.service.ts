6062a3ba1c75cf52756e7044e59d0fb50137a164
wesleysan7os <wesley.santos.comp@gmail.com>
2023-12-07 18:03:32 +0000

    feat: prisma initials & docker config

--- src/assets/assets.service.ts
+++ src/assets/assets.service.ts
@@ -1,4 +1,13 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma/prisma.service';
 
 @Injectable()
-export class AssetsService {}
+export class AssetsService {
+  constructor(private prismaService: PrismaService) {}
+
+  create(data: { id: string; symbol: string; price: number }) {
+    return this.prismaService.asset.create({
+      data,
+    });
+  }
+}
--- src/prisma/prisma/prisma.service.ts
+++ src/prisma/prisma/prisma.service.ts
@@ -8,7 +8,7 @@ export class PrismaService extends PrismaClient implements OnModuleInit {
   }
 
   async enableShutdownHooks(app: any) {
-    this.$on('beforeExit', async () => {
+    this.$on('beforeExit' as never, async () => {
       await app.close();
     });
   }
